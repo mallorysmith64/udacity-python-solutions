eb8f95fa9db4192864fb0f7e833299b058ad9e76
mallorysmith64 <mallorysmith64@gmail.com>
2020-04-04 18:37:07 -0400

    deleted comment

--- CountBy.py
+++ CountBy.py
@@ -13,9 +13,6 @@ end_num = 100
 count_by = 2
 
 break_num = end_num + start_num
-
-# write a while loop that uses break_num as the ongoing number to 
-#   check against end_num
 while  break_num < end_num:
     break_num += count_by
 
